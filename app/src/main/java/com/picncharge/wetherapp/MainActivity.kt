b7588a8d9cbe7bb327734c032bf70200cd919c82
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-11-29 11:22:29 +0530

    UI fix and File Reader added

--- app/src/main/java/com/picncharge/wetherapp/MainActivity.kt
+++ app/src/main/java/com/picncharge/wetherapp/MainActivity.kt
@@ -1,13 +1,57 @@
 package com.picncharge.wetherapp
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.TextView
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btn_continue : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//---------------------------------------- Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+//------------------------------------
         setContentView(R.layout.activity_main)
 
+        btn_continue = findViewById(R.id.btn_continue)
+        var txt_city = findViewById<TextView>(R.id.textView2)
+
+        btn_continue.setOnClickListener(){
+            val file:String = "tempCity.txt"
+            /*
+            val data:String = "colombo"
+            val fileOutputStream: FileOutputStream
+            try {
+                fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)
+                fileOutputStream.write(data.toByteArray())
+                txt_city.setText("Sucess")
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+            */
+            var fileInputStream: FileInputStream? = null
+            fileInputStream = openFileInput(file)
+            var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
+            val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder: StringBuilder = StringBuilder()
+            var text: String? = null
+            while ({ text = bufferedReader.readLine(); text }() != null) {
+                stringBuilder.append(text)
+            }
+            var city = (stringBuilder.toString()).toString()
+            
+        }
 
     }
 }
