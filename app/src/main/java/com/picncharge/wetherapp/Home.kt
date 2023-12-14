3c4845d6499a592ec4bb044dd1447339af760dba
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-12-14 21:53:21 +0530

    api call

--- app/src/main/java/com/picncharge/wetherapp/Home.kt
+++ app/src/main/java/com/picncharge/wetherapp/Home.kt
@@ -1,21 +1,37 @@
 package com.picncharge.wetherapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
+import java.io.FileOutputStream
 
 class Home : AppCompatActivity() {
 
-    lateinit var img : ImageView
+    lateinit var w_s_icon : ImageView
+
+    lateinit var btn_show_more : TextView
+
+    lateinit var txt_city : TextView
+    lateinit var txt_humidity : TextView
+    lateinit var txt_wind_speed : TextView
+    lateinit var txt_temperature : TextView
+    lateinit var txt_weather_condition : TextView
+
+    lateinit var inp_city : EditText
+    lateinit var btn_search : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,33 +44,95 @@ class Home : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         var city = intent.getStringExtra("City")
 
+        txt_city = findViewById(R.id.txt_city)
+        txt_temperature = findViewById(R.id.txt_celcious)
+        txt_humidity = findViewById(R.id.txt_humidity)
+        txt_wind_speed = findViewById(R.id.txt_wind_speed)
+        txt_weather_condition = findViewById(R.id.txt_weather_condition)
+
+        w_s_icon = findViewById(R.id.icon)
+
+        var currentCity = txt
+
+
+        loadCityData(city.toString())
+
+        btn_search = findViewById(R.id.btn_search)
+        btn_show_more = findViewById(R.id.btn_show_more)
+
+        btn_search.setOnClickListener(){
+            inp_city = findViewById(R.id.input_city)
+
+            loadCityData(inp_city.toString())
+
+            updateCurrentCity(inp_city.toString())
+        }
+
+        btn_show_more.setOnClickListener(){
+
+        }
 
         //https://api.openweathermap.org/data/2.5/forecast?q=Colombo&appid=8986a26ec8e3a1442e536cae551826b2
 
     }
 
-    private fun fetchCatImage() {
-        Log.e("API Responce", "Getting Responces")
+    fun loadCityData(city: String) {
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=8986a26ec8e3a1442e536cae551826b2"
 
-        var city = intent.getStringExtra("City")
+        val request = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener { data ->
+            Log.e("Response", data.toString())
 
-        val url = "https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=8986a26ec8e3a1442e536cae551826b2"
-        val request = JsonArrayRequest(
-            Request.Method.GET, url, null,
-            Response.Listener { response ->
-                try {
-                    val imageURL = response.getJSONObject(0).getString("url")
-                    Picasso.get().load(imageURL).into(img)
-                } catch (e: Exception) {
-                    Log.e("Exception", e.toString())
+            try {
+                // Extract temperature from main object (in Kelvin)
+                val temperatureKelvin = data.getJSONObject("main").getDouble("temp")
+
+                // Convert Kelvin to Celsius
+                val temperatureCelsius = temperatureKelvin - 273.15
+                // Display the temperature in Celsius in the TextView
+                txt_temperature.text = String.format("%.2f °C", temperatureCelsius)
+
+                // Extract Weather Description
+                val weatherArray = data.getJSONArray("weather")
+                if (weatherArray.length() > 0) {
+                    val main = weatherArray.getJSONObject(0).getString("main")
+                    val description = weatherArray.getJSONObject(0).getString("description")
+                    val condition = "$main - $description"
+                    txt_weather_condition.text = condition
                 }
-            },
-            Response.ErrorListener { error ->
-                Log.e("Error", error.toString())
-                Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show()
-            })
 
+                // Extract Humidity
+                val humidity = data.getJSONObject("main").getInt("humidity")
+                txt_humidity.text = "$humidity%"
+
+                // Extract Wind Speed
+                val windSpeed = data.getJSONObject("wind").getDouble("speed")
+                txt_wind_speed.text = String.format("%.2f m/s", windSpeed)
+
+                // Load and display weather icon (similar to your existing code)
+                val imageURL = "https://openweathermap.org/img/w/" +
+                        weatherArray.getJSONObject(0).getString("icon") + ".png"
+                Picasso.get().load(imageURL).into(w_s_icon)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }) { error ->
+            Log.e("Response", error.toString())
+        }
         Volley.newRequestQueue(this).add(request)
+    }
+
+    fun updateCurrentCity(city : String){
+        val file:String = "tempCity.txt"
+        val data:String = city
+        val fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)
+            fileOutputStream.write(data.toByteArray())
+            txt_city.setText("Sucess")
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
     }
 
 }
