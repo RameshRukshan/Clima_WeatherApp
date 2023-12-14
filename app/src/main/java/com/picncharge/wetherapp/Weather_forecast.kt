ee395040d46d34634a529163d58b12b4f7c5e110
Ramesh Rukshan <ramesharukshan3@gmail.com>
2023-12-14 22:31:20 +0530

    forcast window

---
+++ app/src/main/java/com/picncharge/wetherapp/Weather_forecast.kt
@@ -0,0 +1,11 @@
+package com.picncharge.wetherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Weather_forecast : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_forecast)
+    }
+}
